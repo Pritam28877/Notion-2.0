b66e2314776fde2caed18c06b5e0eeea5445b4e7
Pritam Das <dpritam28577@gmail.com>
2023-12-01 21:12:26 +0530

    Update configuration and scripts the data base has been working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-  console.error("DATABASE_URL is not set");
+  console.log("🔴 Cannot find database url");
 }
 
 export default {
   schema: "./src/lib/supabase/schema.ts",
-  out: "./migration",
+  out: "./migrations",
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL || "",
