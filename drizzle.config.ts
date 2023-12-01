b71d2cb5c1194a70d8a3823b1d17af5187213f94
Pritam Das <dpritam28577@gmail.com>
2023-12-01 20:21:53 +0530

    Add DATABASE_URL check in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,16 @@
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL is not set");
+}
+
+export default {
+  schema: "./src/lib/supabase/schema.ts",
+  out: "./migration",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL || "",
+  },
+} satisfies Config;
