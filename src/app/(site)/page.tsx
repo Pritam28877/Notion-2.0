4ee40e8b5cb0f67da0e4b48095e91972c989b8d2
Pritam Das <dpritam28577@gmail.com>
2023-12-02 16:43:12 +0530

    Add banner image and section for meeting tracking

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import Banner from "../../../public/appBanner.png";
+import Cal from "../../../public/cal.png";
+
 import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
 const HomePage = () => {
   return (
@@ -115,6 +117,49 @@ const HomePage = () => {
               ))}
             </div>
           ))}
+        </div>
+      </section>
+      <section
+        className="px-4
+        sm:px-6
+        flex
+        justify-center
+        items-center
+        flex-col
+        relative
+      "
+      >
+        <div
+          className="w-[30%]
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-10
+          top-22
+        "
+        />
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        <div
+          className="mt-10
+          max-w-[450px]
+          flex
+          justify-center
+          items-center
+          relative
+          sm:ml-0
+          rounded-2xl
+          border-8
+          border-washed-purple-300 
+          border-opacity-10
+        "
+        >
+          <Image src={Cal} alt="Banner" className="rounded-2xl" />
         </div>
       </section>
     </>
