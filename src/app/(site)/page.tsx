38aff0b66a4a72b8eea6a0bb035323a20dff0b6c
Pritam Das <dpritam28577@gmail.com>
2023-12-03 16:04:33 +0530

    Add trusted testimonials section to homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -162,6 +162,37 @@ const HomePage = () => {
           <Image src={Cal} alt="Banner" className="rounded-2xl" />
         </div>
       </section>
+      <section className="relative">
+        <div
+          className="w-full
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-100
+          top-56
+        "
+        />
+        <div
+          className="mt-20
+          px-4
+          sm:px-6 
+          flex
+          flex-col
+          overflow-x-hidden
+          overflow-visible
+        "
+        >
+          {" "}
+          <TitleSection
+            title="Trusted by all"
+            subheading="Join thousands of satisfied users who rely on our platform for their 
+        personal and professional productivity needs."
+            pill="Testimonials"
+          />
+        </div>
+      </section>
     </>
   );
 };
