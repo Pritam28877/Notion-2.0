f318455f8da91e76d43e473784b3bb16cda9bb5d
Pritam Das <dpritam28577@gmail.com>
2023-12-03 16:56:05 +0530

    Add custom card component and testimonials section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import React from "react";
 import Banner from "../../../public/appBanner.png";
 import Cal from "../../../public/cal.png";
-
+import CustomCard from "@/components/landing-page/custom-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
+import { randomUUID } from "crypto";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 const HomePage = () => {
   return (
     <>
@@ -88,7 +92,7 @@ const HomePage = () => {
           before:absolute
         "
         >
-          {[...Array(2)].map((arr,i) => (
+          {[...Array(2)].map((arr, i) => (
             <div
               key={i}
               className="flex
@@ -97,7 +101,6 @@ const HomePage = () => {
           "
             >
               {CLIENTS.map((client) => (
-                
                 <div
                   key={client.alt}
                   className=" relative
@@ -192,6 +195,52 @@ const HomePage = () => {
         personal and professional productivity needs."
             pill="Testimonials"
           />
+          {[...Array(2)].map((arr, index) => (
+            <div
+              key={randomUUID()}
+              className={twMerge(
+                clsx("mt-10 flex flex-nowrap gap-6 self-start ", {
+                  "flex-row-reverse": index === 1,
+                  "animate-[slide_250s_linear_infinite]": true,
+                  "animate-[slide_250s_linear_infinite_reverse]": index === 1,
+                  "ml-[100vw]": index === 1,
+                }),
+                "hover:paused"
+              )}
+            >
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px]
+                  shrink-0
+                  rounded-xl
+                  dark:bg-gradient-to-t
+                  dark:from-border dark:to-background"
+                  cardHeader={
+                    <div
+                      className="flex
+                      items-center
+                      gap-4
+                  "
+                    >
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                ></CustomCard>
+              ))}
+            </div>
+          ))}
         </div>
       </section>
     </>
