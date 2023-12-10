5e444c829c28b37f69411634194fe90e0388968d
Pritam Das <dpritam28577@gmail.com>
2023-12-10 19:36:13 +0530

    Add navigation menu and list items to header component

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -3,6 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "../../../public/cypresslogo.svg";
 import React, { useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 const routes = [
   { title: "Features", href: "#features" },
@@ -73,9 +83,99 @@ const Header = () => {
             cypress.
           </span>
         </Link>
+        <NavigationMenu className="hidden md:block ">
+          <NavigationMenuList className="gap-6">
+            <NavigationMenuItem>
+              <NavigationMenuTrigger
+                onClick={() => setPath("#resources")}
+                className={cn({
+                  "dark:text-white": path === "#resources",
+                  "dark:text-white/40": path !== "#resources",
+                  "font-normal": true,
+                  "text-xl": true,
+                })}
+              >
+                Resources
+              </NavigationMenuTrigger>
+              <NavigationMenuIndicator>
+                <ul
+                  className="grid
+                gap-3
+                p-6
+                md:w-[400px]
+                ld:w-[500px]
+                lg:grid-cols-[.75fr_1fr]
+                "
+                >
+                  <li className="row-span-3">
+                    <span
+                      className="flex h-full w-full select-none
+                  flex-col
+                  justify-end
+                  rounded-md
+                  bg-gradient-to-b
+                  from-muted/50
+                  to-muted
+                  p-6 no-underline
+                  outline-none
+                  focus:shadow-md
+                  "
+                    >
+                      Welcome
+                    </span>
+                  </li>
+                  <ListItem href="#" title="Introduction">
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </ListItem>
+                  <ListItem href="#" title="Installation">
+                    How to install dependencies and structure your app.
+                  </ListItem>
+                  <ListItem href="#" title="Typography">
+                    Styles for headings, paragraphs, lists...etc
+                  </ListItem>
+                </ul>
+              </NavigationMenuIndicator>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </header>
     </>
   );
 };
 
 export default Header;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "group block select-none space-y-1 font-medium leading-none"
+          )}
+          {...props}
+        >
+          <div className="text-white text-sm font-medium leading-none">
+            {title}
+          </div>
+          <p
+            className="group-hover:text-white/70
+            line-clamp-2
+            text-sm
+            leading-snug
+            text-white/40
+          "
+          >
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+
+ListItem.displayName = "ListItem";
