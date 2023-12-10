deed2db396213cfeb9c83ea1b610a6b1708de290
Pritam Das <dpritam28577@gmail.com>
2023-12-10 19:48:15 +0530

    Add navigation menu items and buttons to header component

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -10,8 +10,10 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuContent,
 } from "../ui/navigation-menu";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"; 
 import { Button } from "../ui/button";
 
 const routes = [
@@ -136,8 +138,84 @@ const Header = () => {
                 </ul>
               </NavigationMenuIndicator>
             </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger
+                onClick={() => setPath("#pricing")}
+                className={cn({
+                  "dark:text-white": path === "#pricing",
+                  "dark:text-white/40": path !== "#pricing",
+                  "font-normal": true,
+                  "text-xl": true,
+                })}
+              >
+                Pricing
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4  md:grid-row-2  ">
+                  <ListItem title="Pro Plan" href={"#"}>
+                    Unlock full power with collaboration.
+                  </ListItem>
+                  <ListItem title={"free Plan"} href={"#"}>
+                    Great for teams just starting out.
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuContent>
+                <ul
+                  className="grid w-[400px]
+              gap-3
+              p-4
+              md:w-[500px]
+              md:grid-cols-2 
+              lg:w-[600px]
+              "
+                >
+                  {components.map((component) => (
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      href={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                className={cn(navigationMenuTriggerStyle(), {
+                  "dark:text-white": path === "#testimonials",
+                  "dark:text-white/40": path !== "#testimonials",
+                  "font-normal": true,
+                  "text-xl": true,
+                })}
+              >
+                Testimonial
+              </NavigationMenuLink>
+            </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
+        <aside
+          className="flex
+        w-full
+        gap-2
+        justify-end
+      "
+        >
+          <Link href={"/login"}>
+            <Button variant="btn-secondary" className=" p-1 hidden sm:block">
+              Login
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button variant="btn-primary" className="whitespace-nowrap">
+              Sign Up
+            </Button>
+          </Link>
+        </aside>
       </header>
     </>
   );
